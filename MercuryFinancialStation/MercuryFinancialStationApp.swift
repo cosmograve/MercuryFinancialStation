7373fe65b6663b99c5a39e9e46e153c5b82808a7
Aver Alexey <68560245+cosmograve@users.noreply.github.com>
2026-03-26 17:50:07 +0700

    Initial Commit

---
+++ MercuryFinancialStation/MercuryFinancialStationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MercuryFinancialStationApp.swift
+//  MercuryFinancialStation
+//
+//  Created by Алексей Авер on 26.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct MercuryFinancialStationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
